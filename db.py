8596b260d09e3fbf10601bfefdbc17dfaf12d6f8
Christopher Partin <114110894+Korachof@users.noreply.github.com>
2024-04-21 15:28:46 -0700

    Update db.py
    Imported json and os
    
    Added a save_file method to save the user's data into a json file. 
    
    Added a load_file method to load the user's data from a json file into the db dictionary. 
    
    Will write tests for these functions and test them soon.

--- db.py
+++ db.py
@@ -1,7 +1,27 @@
+import json
+import os
+
 class DataBase:
   def __init__(self, user, db):
     self._user = user
     self._db = db
+
+  def save_db(self):
+    with open(f"{self._user}.json", "w") as outfile:
+      json.dump(self._db, outfile)
+
+  def load_db(self, user):
+    if os.path.isfile(f"{user}.json") is False:
+      return "The File Does Not Exist"
+    save_file = open(f"{user}.json", "r")
+
+    db_data = json.load(save_file)
+
+    self._user = user
+    self._db = db_data
+    
+    
+      
 
 
     
